d0c85f4fb98a2a3a98279c84515e8a441e8a92e2
augustanius <augustanius@gmail.com>
2023-03-01 21:52:28 +0700

    feat(user): Add user entity and migrations

---
+++ src/Entity/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MyApp\Entity;
+
+class User
+{
+    private $name;
+
+    private $password;
+
+    private $id;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
